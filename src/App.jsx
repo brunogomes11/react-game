09dab57b346da2859dc5f7f0fda4934df2d2e3c6
Joao Murara <joaopaulo.murara@gmail.com>
2023-10-09 20:13:25 +1000

    refactoring styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,15 @@ import Quiz from "./Quiz";
 import { jsQuiz } from "./constant";
 
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-
-//Set a base CSS for BODY
-const theme = createTheme({
-    typography: {
-        fontFamily: "Pixel",
-    },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: {
-                "@font-face": {
-                    fontFamily: "Pixel",
-                    src: `url('PublicPixel-z84yD.ttf') format('truetype')`,
-                },
-                body: {
-                    backgroundColor: "#23143c",
-                    display: "flex",
-                    justifyContent: "center",
-                    margin: "0 auto",
-                    padding: "0 30px",
-                    height: "100vh",
-                },
-            },
-        },
-    },
-});
+import { theme } from "./app_styles.js";
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Quiz questions={jsQuiz.questions} />
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Quiz questions={jsQuiz.questions} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,78 +1,66 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-
+import { boxQuizBackground } from "./quiz_styles";
 import { Box, Typography, List, ListItem } from "@mui/material";
 
 const Quiz = ({ questions }) => {
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [answerIndex, setAnswerIndex] = useState(null); //When someone click on the answer, we can track the index
-    const [answer, setAnswer] = useState(null); //Set answer to true if its the right one
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answerIndex, setAnswerIndex] = useState(null); //When someone click on the answer, we can track the index
+  const [answer, setAnswer] = useState(null); //Set answer to true if its the right one
 
-    const { question, choices, correctAnswer } = questions[currentQuestion];
+  const { question, choices, correctAnswer } = questions[currentQuestion];
 
-    const onAnswerClick = (answer, index) => {
-        setAnswerIndex(index);
-        if (answer === correctAnswer) {
-            setAnswer(true);
-        } else {
-            setAnswer(false);
-        }
-    };
+  const onAnswerClick = (answer, index) => {
+    setAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setAnswer(true);
+    } else {
+      setAnswer(false);
+    }
+  };
+  return (
+    //Box container = div with styles using sx
+    <Box sx={boxQuizBackground}>
+      <Typography //Display the position of the question
+        component="span"
+        color="white"
+        className="active-question-no"
+      >
+        {currentQuestion + 1}
+      </Typography>
 
-    return (
-        //Box container = div with styles using sx
-        <Box
+      <Typography //Display the total question
+        component="span"
+        color="white"
+        className="total-question"
+      >
+        /{questions.length}
+      </Typography>
+
+      <Typography color="white" variant="h6">
+        {question}
+      </Typography>
+
+      <List>
+        {choices.map((answer, index) => (
+          <ListItem
+            onClick={() => onAnswerClick(answer, index)}
+            key={answer}
             sx={{
-                width: 500,
-                backgroundColor: "#0c062e",
-                borderRadius: 4,
-                marginTop: 30,
-                padding: "30px 60px",
-                boxSizing: "border-box",
+              backgroundColor:
+                answerIndex === index ? "secondary.main" : "transparent", // or any other styling
+              color: "white",
+              "&:hover": {
+                backgroundColor: "primary.light",
+              },
             }}
-        >
-            <Typography //Display the position of the question
-                component="span"
-                color="white"
-                className="active-question-no"
-            >
-                {currentQuestion + 1}
-            </Typography>
-
-            <Typography //Display the total question
-                component="span"
-                color="white"
-                className="total-question"
-            >
-                /{questions.length}
-            </Typography>
-
-            <Typography color="white" variant="h6">
-                {question}
-            </Typography>
-
-            <List>
-                {choices.map((answer, index) => (
-                    <ListItem
-                        onClick={() => onAnswerClick(answer, index)}
-                        key={answer}
-                        sx={{
-                            backgroundColor:
-                                answerIndex === index
-                                    ? "secondary.main"
-                                    : "transparent", // or any other styling
-                            color: "white",
-                            "&:hover": {
-                                backgroundColor: "primary.light",
-                            },
-                        }}
-                    >
-                        {answer}
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
+          >
+            {answer}
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 };
 
 export default Quiz;
