6f1ae1bbb80d577dd326c540ed48c2586bee1b91
Joao Murara <joaopaulo.murara@gmail.com>
2023-10-13 21:50:49 +1000

    creating the header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./app_styles.js";
 import Quiz from "./Quiz";
 import SelectCategory from "./SelectCategory.jsx";
+import Header from "./Header.jsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Header />
       <SelectCategory />
       {/* <Quiz /> */}
     </ThemeProvider>
