6462d25783234b980b61988093be323a3dd5848c
Joao Murara <joaopaulo.murara@gmail.com>
2023-10-13 15:09:51 +1000

    creating card elements for sekect category

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./app_styles.js";
 import Quiz from "./Quiz";
+import SelectCategory from "./SelectCategory.jsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Quiz />
+      <SelectCategory />
+      {/* <Quiz /> */}
     </ThemeProvider>
   );
 }
