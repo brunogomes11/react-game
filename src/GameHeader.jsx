26eadb5ab09c226138b5aa6ee88fee6f68b9f0f0
Bruno Gomes <brunogomes1198@gmail.com>
2023-10-16 21:54:14 +1100

    Create dialog for scoreboard

--- src/GameHeader.jsx
+++ src/GameHeader.jsx
@@ -1,22 +1,28 @@
+/* eslint-disable react/prop-types */
 import { Typography, Grid } from "@mui/material";
 
-function GameHeader({ score, currentQuestionIndex }) {
-  return (
-    <Grid container className="game-header">
-      <Grid>
-        <Typography color="white" className="active-question-no">
-          {/* Display the current question number */}
-          Question: {currentQuestionIndex + 1}
-        </Typography>
-      </Grid>
-      <Grid>
-        <Typography color="white" align="center" className="total-score">
-          {/* Display the total score */}
-          Score:{score}
-        </Typography>
-      </Grid>
-    </Grid>
-  );
+function GameHeader({ score, currentQuestionIndex, timer }) {
+    return (
+        <Grid container className="game-header">
+            <Grid>
+                <Typography color="white" className="active-question-no">
+                    {/* Display the current question number */}
+                    Question: {currentQuestionIndex + 1}
+                </Typography>
+            </Grid>
+            <Grid>
+                <Typography
+                    color="white"
+                    align="center"
+                    className="total-score"
+                >
+                    {/* Display the total score */}
+                    Score:{score}
+                </Typography>
+            </Grid>
+            <Grid>{timer}</Grid>
+        </Grid>
+    );
 }
 
 export default GameHeader;
