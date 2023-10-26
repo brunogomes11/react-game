bc203b037d6b52dc6af6b3ec14d8fe93f9083428
Bruno Gomes <brunogomes1198@gmail.com>
2023-10-26 22:42:13 +1100

    Styling input field name

--- src/Question.jsx
+++ src/Question.jsx
@@ -4,144 +4,143 @@ import { useEffect, useState } from "react";
 import { Typography, Grid, Box, Button } from "@mui/material";
 
 const Question = ({
-  nextQuestion,
-  currentQuestion,
-  updateScoreState,
-  decrementLives,
-  resetTimer,
+    nextQuestion,
+    currentQuestion,
+    updateScoreState,
+    decrementLives,
+    resetTimer,
 }) => {
-  const [answer, setAnswer] = useState(null); //set answer to true if its the right one
+    const [answer, setAnswer] = useState(null); //set answer to true if its the right one
 
-  const isCorrect = answer === currentQuestion.correct_answer;
-  console.log(currentQuestion.correct_answer);
-  // const feedback = isCorrect ? "You got it right!" : "Wrong answer!";
-  const isAnswered = answer !== null;
+    const isCorrect = answer === currentQuestion.correct_answer;
 
-  // callback triggered when player selects an answer
-  const onAnswerClick = (choice) => {
-    // updates the element style
-    setAnswer(choice);
+    // const feedback = isCorrect ? "You got it right!" : "Wrong answer!";
+    const isAnswered = answer !== null;
 
-    if (choice === currentQuestion.correct_answer) {
-      updateScoreState();
-      resetTimer();
-    } else {
-      decrementLives();
-    }
+    // callback triggered when player selects an answer
+    const onAnswerClick = (choice) => {
+        // updates the element style
+        setAnswer(choice);
 
-    //Move to next question
-    nextQuestion();
-  };
+        if (choice === currentQuestion.correct_answer) {
+            console.log("Answer is correct!");
+            updateScoreState();
+            resetTimer();
+        } else {
+            decrementLives();
+        }
 
-  useEffect(() => {
-    setAnswer(null);
-  }, [currentQuestion]);
+        //Move to next question
+        nextQuestion();
+    };
 
-  return (
-    <Grid
-      className="questionComponent"
-      container
-      spacing={1}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Grid item className="question">
-        <Typography color="white" variant="h6" align="center">
-          {/* displays the question */}
-          {currentQuestion.question}
-        </Typography>
-      </Grid>
+    useEffect(() => {
+        setAnswer(null);
+    }, [currentQuestion]);
 
+    return (
+        <Grid
+            className="questionComponent"
+            container
+            spacing={1}
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <Grid item className="question">
+                <Typography color="white" variant="h6" align="center">
+                    {/* displays the question */}
+                    {currentQuestion.question}
+                </Typography>
+            </Grid>
 
-      <Grid
-        item
-        className="choices"
-        container
-        spacing={2}
-        marginTop="30px"
-        align="center"
-        sx={{
-          pointerEvents: !isAnswered ? "auto" : "none",
-        }}
-      >
-        {/* list the answer choices */}
-        {currentQuestion.choices.map((choice, index) => (
-          <Grid item xs={6} key={index}>
-            <Button
-              //   callback to check if answer is correct/wrong
-              onClick={(e) => onAnswerClick(e, choice, isCorrect, answer)}
-              sx={{
-                color: "white",
-                width: "80%",
-                height: "80%",
-
-
-                "&:hover": {
-                  backgroundColor: "secondary.dark",
-                  cursor: "pointer",
-                },
-              }}
+            <Grid
+                item
+                className="choices"
+                container
+                spacing={2}
+                marginTop="30px"
+                align="center"
+                sx={{
+                    pointerEvents: !isAnswered ? "auto" : "none",
+                }}
             >
-              <Typography variant="body2" gutterBottom>
-                {choice}
-              </Typography>
-            </Button>
-          </Grid>
-        ))}
-      </Grid>
+                {/* list the answer choices */}
+                {currentQuestion.choices.map((choice, index) => (
+                    <Grid item xs={6} key={index}>
+                        <Button
+                            //   callback to check if answer is correct/wrong
+                            onClick={(e) => onAnswerClick(choice)}
+                            sx={{
+                                color: "white",
+                                width: "80%",
+                                height: "80%",
 
-      <Grid
-        item
-        className="feedback"
-        sx={{
-          position: "absolute",
-          width: "15rem",
-          height: "15rem",
-          padding: "0",
-        }}
-      >
-        {/* {isAnswered && (
+                                "&:hover": {
+                                    backgroundColor: "secondary.dark",
+                                    cursor: "pointer",
+                                },
+                            }}
+                        >
+                            <Typography variant="body2" gutterBottom>
+                                {choice}
+                            </Typography>
+                        </Button>
+                    </Grid>
+                ))}
+            </Grid>
+
+            <Grid
+                item
+                className="feedback"
+                sx={{
+                    position: "absolute",
+                    width: "15rem",
+                    height: "15rem",
+                    padding: "0",
+                }}
+            >
+                {/* {isAnswered && (
           <Typography variant="h6" sx={{ color: isCorrect ? "green" : "red" }}>
             {feedback}
           </Typography>
         )} */}
 
-        {isAnswered &&
-          (isCorrect ? (
-            <Box
-              sx={{
-                backgroundImage: `
+                {isAnswered &&
+                    (isCorrect ? (
+                        <Box
+                            sx={{
+                                backgroundImage: `
                     url("/correctAnswer.png")`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                height: "100%",
-                width: "100%",
-                backgroundColor: "rgba(0, 128, 0, 0.25)",
-                borderRadius: ".2rem",
-              }}
-            ></Box>
-          ) : (
-            <Box
-              sx={{
-                backgroundImage: `
+                                backgroundSize: "contain",
+                                backgroundRepeat: "no-repeat",
+                                backgroundPosition: "center",
+                                height: "100%",
+                                width: "100%",
+                                backgroundColor: "rgba(0, 128, 0, 0.25)",
+                                borderRadius: ".2rem",
+                            }}
+                        ></Box>
+                    ) : (
+                        <Box
+                            sx={{
+                                backgroundImage: `
                 url("/wrongAnswer.png")`,
-                backgroundSize: "contain",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                height: "100%",
-                width: "100%",
-                backgroundColor: "rgba(255, 0, 0, 0.25)",
-                borderRadius: ".2rem",
-              }}
-            ></Box>
-          ))}
-      </Grid>
-    </Grid>
-  );
+                                backgroundSize: "contain",
+                                backgroundRepeat: "no-repeat",
+                                backgroundPosition: "center",
+                                height: "100%",
+                                width: "100%",
+                                backgroundColor: "rgba(255, 0, 0, 0.25)",
+                                borderRadius: ".2rem",
+                            }}
+                        ></Box>
+                    ))}
+            </Grid>
+        </Grid>
+    );
 };
 
 export default Question;
--- src/model.js
+++ src/model.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const scoresTable = new mongoose.Schema({
-  name: {
-    required: true,
-    type: String,
-  },
-  score: {
-    required: true,
-    type: Number,
-  },
+    name: {
+        required: true,
+        type: String,
+        unique: true,
+    },
+    score: {
+        required: true,
+        type: Number,
+    },
 });
 
 module.exports = mongoose.model("scoresCollection", scoresTable);
